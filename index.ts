0377a23b7c01137e7e865d06c0c1a63bc7a90fa0
galihvsx <galihdjx@gmail.com>
2026-02-16 20:01:27 +0800

    feat: add public exports

---
+++ index.ts
@@ -0,0 +1,8 @@
+export { PmanClient } from "./src/lib/PmanClient";
+export { Command, CommandOptions, CommandContext } from "./src/lib/structures/Command";
+export { Listener, ListenerOptions } from "./src/lib/structures/Listener";
+export { Plugin } from "./src/lib/structures/Plugin";
+export { CommandStore } from "./src/lib/stores/CommandStore";
+export { ListenerStore } from "./src/lib/stores/ListenerStore";
+export { PluginStore } from "./src/lib/stores/PluginStore";
+export { command } from "./src/lib/decorators/command";
